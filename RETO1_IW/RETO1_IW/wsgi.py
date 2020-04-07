218ee01fdae34721ff777e79d396d1f195f63f1f
Aitor Ubierna <aitor.ubierna@opendeusto.es>
2020-04-07 18:29:23 +0200

    primera subida

---
+++ RETO1_IW/RETO1_IW/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for RETO1_IW project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RETO1_IW.settings')
+
+application = get_wsgi_application()
---
+++ RETO1_IW/proyecto_colaborativo/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ProyectoColaborativoConfig(AppConfig):
+    name = 'proyecto_colaborativo'
